5d3debda8f7b8139d3d5615efeba5cdd684299e1
Kenneth Wang <khkwang@gmail.com>
2014-01-26 18:44:44 -0500

    Added mongodb and monk to project

--- spewzik/app.js
+++ spewzik/app.js
@@ -7,6 +7,8 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var mongo = require('mongodb');
+var monk = require('monk');
 
 var app = express();
 
@@ -25,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+  var db = monk('localhost:27017/spewzik_dev');
+} else {
+  var db = monk('localhost:27017/spewzik');
 }
 
 app.get('/', routes.index);
