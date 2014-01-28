392155ce5888aa97eaea8876185f82f3ce5990e1
Kenneth Wang <khkwang@gmail.com>
2014-01-28 00:12:18 -0500

    Inserts more metadata from yt to db and downloads video to ./tracks

--- spewzik/routes/index.js
+++ spewzik/routes/index.js
@@ -1,8 +1,47 @@
-var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/spewzik');
 var url = require('url');
 var querystring = require('querystring');
+var xml2js = require('xml2js');
+var youtubedl = require('youtube-dl');
+var request = require('request');
+
+var db = monk('localhost:27017/spewzik');
+
+downloadTrack = function(host, extId) {
+	var dlPath = './tracks';
+	if (host === 'youtube') {
+		var dl = youtubedl.download(
+			'http://www.youtube.com/watch?v=' + extId, 
+			dlPath,
+			['--max-quality=18']);
+
+		dl.on('download', function(data) {
+			console.log('Download started');
+			console.log('filename: ' + data.filename);
+			console.log('size: ' + data.size);
+		});
+
+		dl.on('progress', function(data) {
+			process.stdout.write(data.eta + ' ' + data.percent + '% at ' + data.speed + '\r');
+		});
+
+		dl.on('error', function(err) {
+			throw err;
+		});
+		
+		dl.on('end', function(data) {
+			//bson.BSON.serialize(object, checkKeys, asBuffer, serializeFunctions)
+			console.log('\nDownload finished!');
+			console.log('ID:', data.id);
+			console.log('Filename:', data.filename);
+			console.log('Size:', data.size);
+			console.log('Time Taken:', data.timeTaken);
+			console.log('Time Taken in ms:', + data.timeTakenms);
+			console.log('Average Speed:', data.averageSpeed);
+			console.log('Average Speed in Bytes:', data.averageSpeedBytes);
+		});
+	}
+}
 
 parseUrl = function(urlStr, callback) {
 	var urlObj = url.parse(urlStr);
@@ -43,7 +82,34 @@ addTrackByUrl = function(url, callback) {
 }
 
 addTrack = function(host, extId, callback) {
-	db.get('tracks').insert({ host: host, extId: extId }, callback);
+	if (host === 'youtube') {
+		request('https://gdata.youtube.com/feeds/api/videos/' + extId + '?v=2', function(err, res, body) {
+			if (err) {
+				callback(err);
+			} else if (res.statusCode !== 200) {
+				callback('video not found');
+			} else {
+				xml2js.parseString(body, function (err, result) {
+					if (err) {
+						callback(err);
+					} else {
+						var track = {
+							name: result.entry.title[0],
+							duration: parseInt(result.entry['media:group'][0]['yt:duration'][0]['$'].seconds),
+							rating: 0,
+							playCount: 0,
+							host: host,
+							extId: extId
+						}
+						db.get('tracks').insert(track, callback);
+						downloadTrack(host, extId);
+					}
+				});
+			}
+		});
+	} else {
+		callback('unsupported host');
+	}
 }
 
 getTrack = function(id, callback) {
