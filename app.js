651b64e2bd7db362f81c2e9c77fdc0c02467acbc
Kenneth Wang <khkwang@gmail.com>
2014-06-03 21:59:19 -0400

    upgrade to express 4.0.x

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
  * Module dependencies.
  */
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorhandler = require('errorhandler')
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
@@ -13,20 +17,14 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(function(req, res, next) {
-	req.url = req.url.replace(/\/{2,}/g, '/');
-	app.router(req, res, next);
-});
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
+	app.use(errorhandler());
 }
 
 app.get('/', routes.index);
